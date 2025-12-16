9fb36fa7b80ff28e8ecb9bd40cf65a9e525bb694
Maaz Hashmi <maazhashmi996-pixel>
2025-12-16 12:27:42 +0500

    first commit

---
+++ Script.js
@@ -0,0 +1,55 @@
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slide");
+const leftArrow = document.querySelector(".arrow.left");
+const rightArrow = document.querySelector(".arrow.right");
+
+let index = 0;
+const totalSlides = slides.length;
+let interval;
+
+/* ===== FUNCTIONS ===== */
+function updateSlider() {
+    slider.style.transform = `translateX(-${index * 100}vw)`;
+    slider.style.transition = "transform 0.8s ease-in-out";
+}
+
+function nextSlide() {
+    index++;
+    if (index >= totalSlides) {
+        index = 0;
+    }
+    updateSlider();
+}
+
+function prevSlide() {
+    index--;
+    if (index < 0) {
+        index = totalSlides - 1;
+    }
+    updateSlider();
+}
+
+/* ===== AUTO SLIDE ===== */
+function startAutoSlide() {
+    interval = setInterval(nextSlide, 4000);
+}
+
+function stopAutoSlide() {
+    clearInterval(interval);
+}
+
+/* ===== EVENTS ===== */
+rightArrow.addEventListener("click", () => {
+    stopAutoSlide();
+    nextSlide();
+    startAutoSlide();
+});
+
+leftArrow.addEventListener("click", () => {
+    stopAutoSlide();
+    prevSlide();
+    startAutoSlide();
+});
+
+/* START */
+startAutoSlide();
